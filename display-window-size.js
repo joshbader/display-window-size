6f099ca12992580817fc033c1f725b92e7f59a93
jbader <josh.bader@babyrobot.com>
2016-04-08 10:06:12 -0400

    Adding script.

---
+++ display-window-size.js
@@ -0,0 +1,36 @@
+javascript:(
+  function() {
+    var vals = document.createElement('div'),
+        windowWidth = window.innerWidth,
+        windowHeight = window.innerHeight,
+        doing;
+
+    function fadeOut() {
+      clearTimeout(doing);
+      doing = setTimeout(function() { vals.style.opacity = 0; }, 1000);
+    }
+
+    window.addEventListener('resize', function() {
+      windowWidth = window.innerWidth;
+      windowHeight = window.innerHeight;
+      vals.style.opacity = 1;
+      vals.innerHTML = windowWidth + ' x ' + windowHeight;
+      fadeOut();
+    });
+    vals.className = 'dj-dsiplay-window-size';
+    vals.style.position = 'fixed';
+    vals.style.zIndex = '100000';
+    vals.style.top = '0';
+    vals.style.right = '0';
+    vals.style.padding = '1em';
+    vals.style.color = '#fff';
+    vals.style.font = '14px/1em sans-serif';
+    vals.style.backgroundColor = 'rgba(0,0,0,0.75)';
+    vals.style.borderRadius = '0 0 0 0.5em';
+    vals.style.transition = 'opacity 200ms';
+    vals.innerHTML = windowWidth + ' x ' + windowHeight;
+
+    if (!document.querySelector('.dj-dsiplay-window-size')) document.body.appendChild(vals);
+    fadeOut();
+  }
+)();
